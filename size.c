cc47539c1400a4e47191b3bad31266b78eabf107
DeepChavda05 <139105683+DeepChavda05@users.noreply.github.com>
2023-09-02 00:05:21 +0530

    1/9/2023

---
+++ size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+	char a;
+	char size;
+	size =sizeof (a);
+	printf (" the size is : %c",size);
+	return 0;
+}
+
---
+++ unary.c
@@ -0,0 +1,13 @@
+// write a programe to using a unary operator ++ and --
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the value of a and b:");
+	scanf ("%d%d",&a,&b);
+	a++;
+	printf("\n after increment value of a is:%d", a);
+	b--;
+	printf("\n decrement of b is:%d",b);
+	return 0;
+}
